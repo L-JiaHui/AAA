4ff32f404abae621d6f25bd4af6a5550f10e74fa
L-JiaHui <906333826@qq.com>
2020-04-14 12:01:10 +0800

    AAA第一次提交V1.0

--- src/main/java/com/xkdgx/controller/AdminsController.java
+++ src/main/java/com/xkdgx/controller/AdminsController.java
@@ -1,16 +1,11 @@
 package com.xkdgx.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.xkdgx.entity.Admins;
 import com.xkdgx.service.AdminsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @Controller
