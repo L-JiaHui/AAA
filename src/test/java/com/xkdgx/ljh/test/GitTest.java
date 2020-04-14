a41fcf98550b8db3c94edccedafabc08781a78e6
L-JiaHui <906333826@qq.com>
2020-04-14 21:29:35 +0800

    测试v5.0

--- src/test/java/com/xkdgx/ljh/test/GitTest.java
+++ src/test/java/com/xkdgx/ljh/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello two");
         System.out.println("21");
+        System.out.println("哈哈哈");
     }
 }
