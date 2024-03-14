dd3b202b444bd933df4a296d7c43c9ad90b3123b
纳兰雨天 <63582034+L-JiaHui@users.noreply.github.com>
2024-03-14 10:45:23 +0800

    Delete src/main/java/com/xkdgx directory

--- src/main/java/com/xkdgx/dao/AdminsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.xkdgx.dao;
-
-import com.xkdgx.entity.Admins;
-
-import java.util.List;
-
-public interface AdminsDAO {
-    List<Admins> showAll();
-}
--- src/main/java/com/xkdgx/service/AdminsService.java
+++
@@ -1,9 +0,0 @@
-package com.xkdgx.service;
-
-import com.xkdgx.entity.Admins;
-
-import java.util.List;
-
-public interface AdminsService {
-    List<Admins> showAll();
-}
